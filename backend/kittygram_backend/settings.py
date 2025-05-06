cf2fa500c08d128abd528ed4bf63910fdc0d26b9
Sergey Vorobyev <vorobjev.s.g@gmail.com>
2025-05-06 12:47:00 +0300

    v2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -96,9 +96,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/backend_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/app/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
