46ab5b10749db29d198ec6b04ef1a2ca9832b149
Sergey Vorobyev <vorobjev.s.g@gmail.com>
2025-05-07 13:30:50 +0300

    fixed reviewer's comments

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,8 +4,6 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
-
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '158.160.12.114', 'kittygramserg.zapto.org']
